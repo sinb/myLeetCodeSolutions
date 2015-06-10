031b17fa38f3d1de196447f4e9731fcc0144978d
sinb <linkin816@gmail.com>
2015-06-10 11:18:28 +0800

    use two pointers and a fake head

--- RemoveNthNode.cpp
+++ RemoveNthNode.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-//struct ListNode
-//{
-//    int val;
-//    ListNode *next;
-//    ListNode(int x) : val(x), next(NULL) {}
-//};
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
 class Solution {
 public:
@@ -37,3 +37,29 @@ public:
 
     }
 };
+//class Solution  // use two pointers and a fake head
+//public:
+//    ListNode* removeNthFromEnd(ListNode* head, int n) {
+//        if (head==NULL || n<=0){
+//            return NULL;
+//        }
+//
+//        ListNode fakeHead(-1);
+//        fakeHead.next = head;
+//        ListNode* conductor1 = &fakeHead;
+//        ListNode* conductor2 = &fakeHead;
+//        for (int i = 0; i < n; i++) {
+//            conductor1 = conductor1->next;
+//        }
+//
+//        while(conductor1->next) {
+//            conductor1 = conductor1->next;
+//            conductor2 = conductor2->next;
+//
+//        }
+//        ListNode* tobeDeleted = conductor2->next;
+//        conductor2->next = conductor2->next->next;
+//        delete tobeDeleted;
+//        return fakeHead.next;
+//    }
+//};
