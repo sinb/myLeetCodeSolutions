e33a09fad4e22095ecdd6a3a51d5d25b3cf622a9
sinb <linkin816@gmail.com>
2015-07-01 10:30:00 +0800

    add findBrother function

--- stringContain.py
+++ stringContain.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jun 30 23:13:57 2015
-这个题头偷个懒用python吧,不会c++的哈系表
+这个题偷个懒用python吧,不会c++的哈系表
+findBrother return brother string of a given string in a list 
 @author: hehe
 """
 
@@ -16,7 +17,30 @@ def stringContain(a, b):
         if dic.get(char,0) == 0:
             return False
     return True            
-    
+
+def findBrother(a, listofwords):                      
+    def getDic(word):
+        dic = {}
+        for char in word:
+            if dic.get(char, 0) == 0:
+                dic[char] = 0            
+                dic[char] += 1
+            else:
+                dic[char] += 1
+        return dic      
+    dic1 = getDic(a)
+    for word in listofwords:
+        dic2 = getDic(word)
+        if dic2 == dic1 and word != a:
+            return word
+        else:
+            pass
+    return None        
+            
 if __name__ == '__main__'    :
     result = stringContain('qqabcde', 'xab')
     print result
+    a = 'abcx'
+    listofwords = ['abcd', 'cbax']
+    a_brother = findBrother(a, listofwords)
+    print a_brother
