c64c5e89228ffc74191ef8393b213cc138d62504
sinb <linkin816@gmail.com>
2015-07-08 10:51:12 +0800

    add solution2

--- powerOfTwo.cpp
+++ powerOfTwo.cpp
@@ -6,6 +6,7 @@ using namespace std;
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+    // just divide 2 to see if n%2 == 0
         if (n < 1)
             return false;
         while (n > 1) {
@@ -18,8 +19,30 @@ public:
         }
         return true;
     }
+    bool isPowerOfTwo2(int n) {
+    // use bit operation, number that is power of 2 only has one bit 1, like 8 = 0000 1000
+        if (n < 1)
+            return false;
+        int sum = 0;
+        int i = 0;
+
+        for (i = 0; i < sizeof(n)*8 && sum <=1; ++i) {
+            sum += ((n >> i) & 1);
+        }
+        if (i < sizeof(n)*8)
+            return false;
+        else
+            return true;
+    }
 };
 int main() {
     Solution s;
-    cout << s.isPowerOfTwo(16);
+    cout << s.isPowerOfTwo2(33) << endl;
+//    int number = 8;
+//    number |= 1 << 1;
+//    cout << number << endl;
+//    int bit;
+//    bit = (number >> 0) & 1;
+//    cout << bit << endl;
+//    cout << sizeof(number) << endl;
 }
