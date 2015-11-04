2433675c9154ec2030f8ce472937eed71e3b720c
sinb <linkin816@gmail.com>
2015-11-04 08:46:59 +0800

    两个数的和,用辅助数组和头尾指针两种方法

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,8 +1,15 @@
+#include <algorithm>
 #include<iostream>
 #include<vector>
 using namespace std;
 class Solution {
 public:
+//用一个辅助数组,存下 target - nums[i]
+//然后一个指针从头,一个从尾扫描
+//如果碰到nums[i] == aux[j]的,说明找到了一组,可以停下了
+//比如nums: 6, 2, 9, 8, 16, target是10
+//   aux : 4, 8, 1, 2, -6
+//可以发现nums[1] = aux[3] == 2,找到了
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
         int auxArray[nums.size()];
@@ -15,6 +22,33 @@ public:
                     result.push_back(j+1);
                     return result;
                 }
+        }
+        return result;
+    }
+//还有一种不需要辅助数组的,先排序
+//然后还是从头和末尾扫描,
+//通过判断当前的current sum是比target大还是小,移动begin或者end,直到begin不小于end或者找到为止
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        vector<int> result;
+        sort(nums.begin(), nums.end());
+        size_t begin = 0;
+        size_t end = nums.size() - 1;
+        while (begin < end)
+        {
+            int currentSum = nums[begin] + nums[end];
+            if (currentSum == target)
+            {
+                    result.push_back(nums[begin]);
+                    result.push_back(nums[end]);
+                    return result;
+            }
+            else
+            {
+                if (currentSum > target)
+                    --end;
+                else
+                    ++begin;
+            }
         }
         return result;
     }
