d31fbdaf13b60436abbbd328499972d03372744f
sinb <linkin816@gmail.com>
2015-12-07 14:14:25 +0800

    移动0, 两个指针

--- MoveZeroes.cpp
+++ MoveZeroes.cpp
@@ -6,6 +6,28 @@ using namespace std;
 //Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 //
 //For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+//python
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        count = 0
+        for i in range(0, len(nums)):
+            if nums[i] != 0:
+                nums[count] = nums[i]
+                count += 1
+        while count < len(nums):
+            nums[count] = 0
+            count += 1
+            
+nums = [2,0,3,1,0,4]        
+s = Solution()
+s.moveZeroes(nums)
+print nums
+//
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
